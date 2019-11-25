4695b63ec6907725ed0bd36453920f74e7371d00
José Luis Ferrete <josel.ferrete@s-dos.es>
2019-11-25 11:02:29 +0100

    Http petitions introduction

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  paises: any[] = [];
+
+  constructor( private http: HttpClient) {
+
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+    .subscribe( (resp: any) => {
+      this.paises = resp;
+      console.log(resp);
+    })
+
+   }
 
   ngOnInit() {
   }
